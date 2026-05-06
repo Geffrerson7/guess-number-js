58fae64918d00967d8cf798f5970e524e0f1435e
Gefferson Casasola <gefferson.casasola@outlook.com>
2026-05-06 11:46:10 -0500

    feat: variables, tipos de datos e interacción con usuario

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 console.log('¡Hola desde JavaScript!');
+// --- Variables con let (pueden cambiar) ---
+let nombre = 'Gefferon Casasola';
+let edad = 32;
+let estaAprendiendo = true;
+
+// --- Imprime cada variable ---
+console.log('Nombre: ', nombre);
+console.log('Edad: ',edad);
+console.log('Está aprendiendo: ', estaAprendiendo);
+
+// --- Verifica los tipos de dato ---
+console.log('Tipo de dato de nombre: ', typeof nombre); 
+console.log('Tipo de dato de edad: ', typeof edad);
+console.log('Tipo de dato de estaAprendiendo: ', typeof estaAprendiendo);
+
+// --- Variables con const (no pueden cambiar) ---
+const curso = 'Code 101';
+const maxIntentos = 10;
+console.log('Curso: ',curso);
+console.log('Máximo número de intentos: ', maxIntentos);
+
+// --- Entrada del usuario ---
+let nombreUsuario = prompt('¿Cómo te llamas?');
+let edadUsuario = prompt('¿Qué edad tienes?');
+
+// --- Salida al usuario ---
+alert("Hola " + nombreUsuario + ", tienes " + edadUsuario + " años.");
+
+// --- Verifica en consola ---
+console.log('Nombre: ', nombreUsuario);
+console.log('Edad: ', edadUsuario);
+console.log('Tipo de edad: ', typeof edadUsuario);
