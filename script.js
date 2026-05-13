25049b39a3722ceddb7694d630640b2afcd1bd76
Gefferson Casasola <gefferson.casasola@outlook.com>
2026-05-13 14:48:33 -0500

    feat: agregar panel de historial de intentos

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // Elementos
 const systemState = document.getElementById("systemState");
+const historyList = document.getElementById("historyList");
 
 function cambiarEstado(texto) {
   systemState.textContent = texto;
+}
+
+function agregarAlHistorial(numero) {
+  const item = document.createElement("div");
+  item.classList.add("history-item");
+  item.textContent = `Intento: ${numero}`;
+  historyList.appendChild(item);
 }
