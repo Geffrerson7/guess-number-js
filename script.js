acb071ae152ba56982598d30a633e1c96552c31a
Gefferson Casasola <gefferson.casasola@outlook.com>
2026-05-13 14:54:19 -0500

    feat: agregar input wrapper con cursor falso estilo terminal

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // Elementos
 const systemState = document.getElementById("systemState");
 const historyList = document.getElementById("historyList");
+const input = document.getElementById("inputIntento");
+const fakeCursor = document.getElementById("fakeCursor");
 
 function cambiarEstado(texto) {
   systemState.textContent = texto;
@@ -11,4 +13,14 @@ function agregarAlHistorial(numero) {
   item.classList.add("history-item");
   item.textContent = `Intento: ${numero}`;
   historyList.appendChild(item);
+}
+
+function actualizarCursor() {
+  if (input.value.length === 0) {
+    fakeCursor.style.left = "calc(50% - 20px)";
+    return;
+  }
+
+  const offset = input.value.length * 18;
+  fakeCursor.style.left = `calc(50% + ${offset - 20}px)`;
 }
