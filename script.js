a1d641e68dff194549686b318cc45743f72129ef
Gefferson Casasola <gefferson.casasola@outlook.com>
2026-05-13 14:56:50 -0500

    feat: agregar botón VERIFICAR CÓDIGO y lógica de validación básica

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const systemState = document.getElementById("systemState");
 const historyList = document.getElementById("historyList");
 const input = document.getElementById("inputIntento");
 const fakeCursor = document.getElementById("fakeCursor");
+const btnVerificar = document.getElementById("btnVerificar");
 
 function cambiarEstado(texto) {
   systemState.textContent = texto;
@@ -24,3 +25,32 @@ function actualizarCursor() {
   const offset = input.value.length * 18;
   fakeCursor.style.left = `calc(50% + ${offset - 20}px)`;
 }
+
+function validarIntento() {
+  const intento = parseInt(input.value);
+
+  if (!input.value) {
+    statusText.textContent = "CÓDIGO NO DETECTADO";
+    return;
+  }
+
+  if (intento < 1 || intento > 100) {
+    statusText.textContent = "FUERA DE RANGO";
+    input.value = "";
+    actualizarCursor();
+    return;
+  }
+
+  if (intento === numeroSecreto) {
+    statusText.textContent = "ACCESO AUTORIZADO";
+  } else if (intento < numeroSecreto) {
+    statusText.textContent = "EL CÓDIGO ES MAYOR";
+  } else {
+    statusText.textContent = "EL CÓDIGO ES MENOR";
+  }
+
+  input.value = "";
+  actualizarCursor();
+}
+
+btnVerificar.addEventListener("click", validarIntento);
